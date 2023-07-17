554fa30e986368fc584e95af5f9da3e925e6f798
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2023-07-17 06:24:51 +0200

    callback

--- index.js
+++ index.js
@@ -1,80 +1,22 @@
-let deckId;
-let computerScore = 0;
-let myScore = 0;
-const cardsContainer = document.getElementById("cards");
-const newDeckBtn = document.getElementById("new-deck");
-const drawCardBtn = document.getElementById("draw-cards");
-const header = document.getElementById("header");
-const remainingText = document.getElementById("remaining");
-const computerScoreEl = document.getElementById("computer-score");
-const myScoreEl = document.getElementById("my-score");
-
-function handleClick() {
+function fetchFunction() {
   fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
     .then((res) => res.json())
     .then((data) => {
-      remainingText.textContent = `Remaining cards: ${data.remaining}`;
-      deckId = data.deck_id;
-      console.log(deckId);
+      console.log(data);
     });
 }
 
-newDeckBtn.addEventListener("click", handleClick);
+document.getElementById("new-deck").addEventListener("click", fetchFunction);
 
-drawCardBtn.addEventListener("click", () => {
-  fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
-    .then((res) => res.json())
-    .then((data) => {
-      remainingText.textContent = `Remaining cards: ${data.remaining}`;
-      cardsContainer.children[0].innerHTML = `
-    <img src=${data.cards[0].image} class="card"/>
-    `;
-      cardsContainer.children[1].innerHTML = `
-    <img src=${data.cards[1].image} class="card"/>
-    `;
-      const winnerText = determineCardWinner(data.cards[0], data.cards[1]);
-      header.textContent = winnerText;
-      if (data.remaining === 0) {
-        drawCardBtn.disabled = true;
-        if (computerScore > myScore) {
-          header.textContent = "The computer won the game!";
-        } else if (myScore > computerScore) {
-          header.textContent = "You won the game!";
-        } else {
-          header.textContent = "It's a tie game!";
-        }
-      }
-    });
-});
+let callBack = () => {
+  console.log("I finally ran!");
+};
 
-function determineCardWinner(card1, card2) {
-  const valueOptions = [
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "JACK",
-    "QUEEN",
-    "KING",
-    "ACE",
-  ];
-  const card1ValueIndex = valueOptions.indexOf(card1.value);
-  const card2ValueIndex = valueOptions.indexOf(card2.value);
+setTimeout(callBack, 2000);
 
-  if (card1ValueIndex > card2ValueIndex) {
-    computerScore++;
-    computerScoreEl.textContent = `Computer score: ${computerScore}`;
-    return "Computer wins!";
-  } else if (card1ValueIndex < card2ValueIndex) {
-    myScore++;
-    myScoreEl.textContent = `My score: ${myScore}`;
-    return "You win!";
-  } else {
-    return "War!";
-  }
-}
+const people = [
+  { name: "Jack", hasPet: true },
+  { name: "Jill", hasPet: false },
+  { name: "Alice", hasPet: true },
+  { name: "Bob", hasPet: false },
+];
