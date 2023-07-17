8694b93569aefa1f49717a57954d0ca42e839843
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2023-07-17 18:09:50 +0200

    css, draw button

--- async.js
+++ async.js
@@ -13,3 +13,19 @@ function handleClick() {
 }
 
 document.getElementById("new-deck").addEventListener("click", handleClick);
+
+document.getElementById("draw-cards").addEventListener("click", () => {
+  fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data.cards);
+      cardComputer = data.cards[0].image;
+      myCard = data.cards[1].image;
+      document.querySelector(".card-slot").innerHTML = `
+        <img src=${cardComputer} />
+      <img src=${myCard} />
+      `;
+    });
+});
+
+//
