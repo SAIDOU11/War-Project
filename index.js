cf85aeb35c5cadbf802dd15804227eeb36089b6c
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2023-07-22 06:27:19 +0200

    practice async & callback

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+const newDeckBtn = document.getElementById("new-deck");
 
+async function handleClick() {
+  const res = await fetch(
+    "https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/"
+  );
+  const data = await res.json();
+  console.log(data);
+}
+
+newDeckBtn.addEventListener("click", handleClick);
+
+const people = [
+  { name: "Jack", hasPet: true, age: 12 },
+  { name: "Jill", hasPet: false, age: 18 },
+  { name: "Alicce", hasPet: true, age: 22 },
+  { name: "Bob", hasPet: false, age: 32 },
+];
+
+function filterArray(array, callback) {
+  const resultingArray = [];
+  for (let item of array) {
+    const result = callback(item);
+    if (result) {
+      resultingArray.push(item);
+    }
+  }
+  return resultingArray;
+}
